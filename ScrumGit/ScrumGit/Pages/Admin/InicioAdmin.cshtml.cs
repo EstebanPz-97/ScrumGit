0aad6a51d92b3aa96a56c567947a93147bb5091f
juan <juanpangel08@hotmail.com>
2022-08-04 23:21:01 -0500

    Se agrego carpetas y sus vistas

---
+++ ScrumGit/ScrumGit/Pages/Admin/InicioAdmin.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace ScrumGit.Pages.AdminProductos
+{
+    public class InicioAdminModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
