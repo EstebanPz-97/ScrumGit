790ece2fffc1da81c4fd6a0ca7c9413076259ce6
Dannydimate <99456287+Dannydimate@users.noreply.github.com>
2022-08-05 21:41:12 -0500

    Commit login

--- ScrumGit/ScrumGit/Pages/cuenta/login.cshtml.cs
+++ ScrumGit/ScrumGit/Pages/cuenta/login.cshtml.cs
@@ -5,6 +5,9 @@ using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Threading.Tasks;
+using ScrumGit.Models;
+using Microsoft.EntityFrameworkCore;
+using ScrumGit.Datos;
 
 
 
@@ -17,10 +20,32 @@ namespace ScrumGit.Pages.cuenta
         [BindProperty]
         public credencial? credencial { get; set; }
 
+        public usuario? usuario { get; set; }
+
 
         public void OnGet()
         {
         }
+
+        private readonly ApplicationDbContext _contexto;
+
+        public loginModel(ApplicationDbContext contexto)
+        {
+            _contexto = contexto;
+        }
+
+        public async Task<IActionResult> OnPost()
+        {
+           usuario = _contexto.usuario.FromSqlRaw("select * from usuario where correo={0} and password={1}",credencial.usuario,credencial.password).FirstOrDefault();
+
+            if (usuario != null && usuario.Idusuario != 0)
+            {
+                return RedirectToPage("/Admin/inicioAdmin");
+            }else {
+                return RedirectToPage("/cuenta/login");
+            }
+        }
+
 
     }
 
@@ -33,7 +58,5 @@ namespace ScrumGit.Pages.cuenta
         [DataType(DataType.Password)]
         public string? password { get; set; }
     }
-
-
 
 }
